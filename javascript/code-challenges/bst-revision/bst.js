43d92ce6aa7f42104a8222c04b0bec290100a721
JalalHasan-22 <jhasanj8@gmail.com>
2022-05-29 14:11:16 +0300

    BST Review

---
+++ javascript/code-challenges/bst-revision/bst.js
@@ -0,0 +1,14 @@
+"use strict";
+
+const Node = require("./node");
+
+class BinarySearchTree {
+  constructor(root = null) {
+    this.root = root;
+  }
+
+  // Traversing the tree
+  // 1) Depth First traversal
+  // a) pre-order
+  preOrder() {}
+}
